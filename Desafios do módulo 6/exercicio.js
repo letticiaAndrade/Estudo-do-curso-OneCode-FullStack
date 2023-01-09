824e1595ed8bfb755434b3958c99c909f338812b
Leticia <leticiaae@hotmail.com.br>
2023-01-09 11:53:48 -0300

    Primeiro desafio de JavaScript

---
+++ Desafios do módulo 6/exercicio.js
@@ -0,0 +1,5 @@
+alert ("Bem-vindo! A seguir pediremos que informe alguns dados.");
+var nome = prompt("Qual o seu nome?");
+var idade = prompt("Qual sua idade?");
+console.log(nome);
+console.log(idade);
