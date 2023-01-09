829379a04058bbc62fc28a9b7a0f431aaab1dde1
Leticia <leticiaae@hotmail.com.br>
2023-01-09 15:04:42 -0300

    usando JavaScript Externo

---
+++ Desafios do módulo 6/exercicio02.js
@@ -0,0 +1,9 @@
+
+var nomeMaisVelha = prompt("Qual o nome da pessoa mais velha? ");
+var idadeMaisVelha = prompt("Qual a idade da pessoa mais velha? ");
+var nomeMaisNova = prompt("Qual o nome da pessoa mais nova? ");
+var idadeMaisNova = prompt("Qual a idade da pessoa mais nova? ");
+var diferenca = idadeMaisVelha - idadeMaisNova;
+alert("A pessoa mais velha tem "+ idadeMaisVelha + " anos e seu nome é " + nomeMaisVelha);
+alert("A pessoa mais nova tem "+ idadeMaisNova + " anos e seu nome é " + nomeMaisNova);
+alert ("A diferença de idade é de: " +diferenca+" ano (s)");
