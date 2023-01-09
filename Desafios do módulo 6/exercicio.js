42a1b2a4ee4791b6f43c03a62981c39561f8c348
Leticia <leticiaae@hotmail.com.br>
2023-01-09 14:15:13 -0300

    alterações

--- Desafios do módulo 6/exercicio.js
+++ Desafios do módulo 6/exercicio.js
@@ -1,5 +1,7 @@
 alert ("Bem-vindo! A seguir pediremos que informe alguns dados.");
 var nome = prompt("Qual o seu nome?");
 var idade = prompt("Qual sua idade?");
-console.log(nome);
-console.log(idade);
+var confirmIdade = confirm("Sua idade é " + idade + " anos ");
+alert("Nome: "+ nome);
+alert("Idade: "+ idade);
+alert("Idade confirmada: "+ confirmIdade);
